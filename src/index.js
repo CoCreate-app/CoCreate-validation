d5206351d84f9e5f48da2c222ad22c88177ef931
Frank Pagan <frank@cocreate.app>
2021-10-12 20:09:50 +0000

    feat: render error message using @cocreate/render

--- src/index.js
+++ src/index.js
@@ -1,27 +1,58 @@
 /********************************************************************************
- * Copyright (C) 2020 CoCreate LLC and others.
+ * Copyright (C) 2020 CoCreate LLC.
  *
  *
  * SPDX-License-Identifier: MIT
  ********************************************************************************/
+
+/*globals CustomEvent*/
 import action from '@cocreate/action';
+import render from '@cocreate/render';
 
 function  validate(btn) {
 	let element = btn.closest('form');
 	let validateElements = element.querySelectorAll('[unique="false"]');
 	
-	if (validateElements){
-	    console.log(validateElements);
+	if (validateElements.length){
+		let data = {
+	        type: 'unique',
+	        status: 'failed',
+	        message: 'One or more values are not unique'
+	    };
+	
+		render.data({
+			selector: "[template_id='validate']",
+			data: data
+		});
+	    console.log(data);
+
 	} 
 	else {
-	    document.dispatchEvent(new event('validated'));
+		document.dispatchEvent(new CustomEvent('validate', {
+			detail: {}
+		}));
 	}
 }
 
+function  removeElement(btn) {
+	let element = btn.closest('[templateid]');
+    if (element)
+        element.remove();
+}
+
+
 action.init({
 	action: "validate",
-	endEvent: "validated",
+	endEvent: "validate",
 	callback: (btn, data) => {
 		validate(btn);
+	}
+});
+
+action.init({
+	action: "remove",
+	endEvent: "remove",
+	callback: (btn, data) => {
+		removeElement(btn);
 	}
 });
