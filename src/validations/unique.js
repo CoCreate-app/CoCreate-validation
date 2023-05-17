78818675c626ac1c414fc01291bf3f7eefc3b53f
frankpagan <frank@cocreate.app>
2023-05-17 16:40:43 -0600

    formating

--- src/validations/unique.js
+++ src/validations/unique.js
@@ -1,33 +1,24 @@
 import render from '@cocreate/render';
 
-export var unique = (elements) => {
+export const unique = (elements) => {
     let failedElements = [];
-    for (let element of elements){
+    for (let element of elements) {
         let unique = element.getAttribute('unique');
-        if (unique == 'false'){
+        if (unique == 'false') {
             failedElements.push(element);
         }
-        // if (unique != 'false'){
-        //     element.setAttribute('validation', 'true');
-        // }
-        // else {
-        //     element.setAttribute('validation', 'false');
-        //     failedElements.push(element);
-        // }
     }
-    if (failedElements.length){
-		render.data({
-			selector: "[template='validate']",
-			data: {
-		        type: 'unique',
-		        status: 'failed',
-		        message: 'One or more values are not unique',
-		        selector: '[unique="false"]',
-		        elements: failedElements
-	    	}
-		});
-		return failedElements;
+    if (failedElements.length) {
+        render.data({
+            selector: "[template='validate']",
+            data: {
+                type: 'unique',
+                status: 'failed',
+                message: 'One or more values are not unique',
+                selector: '[unique="false"]',
+                elements: failedElements
+            }
+        });
+        return failedElements;
     }
 };
-
-// unique();
