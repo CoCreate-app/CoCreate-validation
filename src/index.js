9265e4a43754527ef4b2d11afba07a5666a47416
frankpagan <frank@cocreate.app>
2023-06-21 20:29:01 -0600

    fix: updated actions callback data object

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@
 /*globals CustomEvent*/
 import action from '@cocreate/actions';
 // import render from '@cocreate/render';
-import {matches} from './validations/matches';
-import {required} from './validations/required';
-import {unique} from './validations/unique';
-import {subdomain} from './validations/subdomain';
+import { matches } from './validations/matches';
+import { required } from './validations/required';
+import { unique } from './validations/unique';
+import { subdomain } from './validations/subdomain';
 
 const validations = new Map();
 validations.set('matches', matches);
@@ -22,61 +22,61 @@ validations.set('unique', unique);
 const selectors = '[matches], [required], [unique], [subdomain]';
 
 function validate(btn, params) {
-	let validateElements;
-	let validateSelectors = [];
-	let failedElements = [];
-	
-	if (params){
-		let selector = params
-		for (let [validation] of validations){
-			let vSelctor = `${selector}[${validation}]`;
-			validateSelectors.push(vSelctor);
-		}
-		let validSelectors = validateSelectors.join(', ');
-		validateElements = document.querySelectorAll(validSelectors);
-		if (!validateElements.length){
-			let element = document.querySelector(selector);
-			if (element)
-				validateElements = element.querySelectorAll(selectors);
-		}
-	} else {
-		let element = btn.closest('form');
-		validateElements = element.querySelectorAll(selectors);
-	}
-	
-	
-	for (let [validation, func] of validations){
-		let elements = [];
-		for (let element of validateElements){
-			// let validationSelectors = element.getAttribute('validate').replace(/ /g, '').split(',');
-			if (element.hasAttribute(validation)){
-				elements.push(element);	
-			}
-		}
-		if (elements.length){
-			let failedEls = func(elements);
-			if (failedEls)
-				failedElements.push(failedEls);
-		}
-	}
-	for (let element of validateElements){
-		let isValid = element.getAttribute('validation');
-		if (isValid == 'false')
-			failedElements.push(element);
-	}
-	
-	if (failedElements.length == 0){
-		document.dispatchEvent(new CustomEvent('validate', {
-			detail: {}
-		}));
-	} 
+    let validateElements;
+    let validateSelectors = [];
+    let failedElements = [];
+
+    if (params) {
+        let selector = params
+        for (let [validation] of validations) {
+            let vSelctor = `${selector}[${validation}]`;
+            validateSelectors.push(vSelctor);
+        }
+        let validSelectors = validateSelectors.join(', ');
+        validateElements = document.querySelectorAll(validSelectors);
+        if (!validateElements.length) {
+            let element = document.querySelector(selector);
+            if (element)
+                validateElements = element.querySelectorAll(selectors);
+        }
+    } else {
+        let element = btn.closest('form');
+        validateElements = element.querySelectorAll(selectors);
+    }
+
+
+    for (let [validation, func] of validations) {
+        let elements = [];
+        for (let element of validateElements) {
+            // let validationSelectors = element.getAttribute('validate').replace(/ /g, '').split(',');
+            if (element.hasAttribute(validation)) {
+                elements.push(element);
+            }
+        }
+        if (elements.length) {
+            let failedEls = func(elements);
+            if (failedEls)
+                failedElements.push(failedEls);
+        }
+    }
+    for (let element of validateElements) {
+        let isValid = element.getAttribute('validation');
+        if (isValid == 'false')
+            failedElements.push(element);
+    }
+
+    if (failedElements.length == 0) {
+        document.dispatchEvent(new CustomEvent('validate', {
+            detail: {}
+        }));
+    }
 }
 
 action.init({
-	name: "validate",
-	callback: (btn, params) => {
-		validate(btn, params);
-	}
+    name: "validate",
+    callback: (data) => {
+        validate(data.element, data.params);
+    }
 });
 
-export default {validations};
+export default { validations };
