c950a3215536eef27f1110d3fe10466c8d9c346e
frankpagan <frank@cocreate.app>
2023-12-15 21:28:24 -0600

    feat: email validation

---
+++ src/validations/email.js
@@ -0,0 +1,31 @@
+import render from '@cocreate/render';
+
+export var email = async (elements) => {
+    let failedElements = [];
+    for (let element of elements) {
+        let length = element.getAttribute('length') || 0
+        length = parseFloat(length)
+        let value = await element.getValue();
+        let isValidEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value);
+
+        if (isValidEmail) {
+            element.setAttribute('validation', 'true');
+        } else {
+            element.setAttribute('validation', 'false');
+            failedElements.push(element);
+        }
+    }
+    if (failedElements.length) {
+        render.render({
+            selector: "[template='validate']",
+            data: [{
+                type: 'email',
+                status: 'failed',
+                message: 'Please enter a valid email address',
+                selector: '[required][validation="false"]',
+                elements: failedElements
+            }]
+        });
+        return failedElements;
+    }
+};
