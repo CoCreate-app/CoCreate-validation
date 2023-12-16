591b2b811d597a909c6d0eb0020ca6857224d66d
frankpagan <frank@cocreate.app>
2023-12-15 21:28:00 -0600

    feat: length validation

---
+++ src/validations/length.js
@@ -0,0 +1,31 @@
+import render from '@cocreate/render';
+
+export var length = async (elements) => {
+    let failedElements = [];
+    for (let element of elements) {
+        let length = element.getAttribute('length') || 0
+        length = parseFloat(length)
+        let value = await element.getValue();
+        if (element.type === 'password')
+            value = atob(value)
+        if (value.length >= length) {
+            element.setAttribute('validation', 'true');
+        } else {
+            element.setAttribute('validation', 'false');
+            failedElements.push(element);
+        }
+    }
+    if (failedElements.length) {
+        render.render({
+            selector: "[template='validate']",
+            data: [{
+                type: 'length',
+                status: 'failed',
+                message: 'Does not meet the minimum character length requirement',
+                selector: '[required][validation="false"]',
+                elements: failedElements
+            }]
+        });
+        return failedElements;
+    }
+};
