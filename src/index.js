e415c5fb8da8e1892cdb530d606c8453e2f1d158
frankpagan <frank@cocreate.app>
2025-02-27 12:55:50 -0600

    fix: dispatch end event on action element rather than document

--- src/index.js
+++ src/index.js
@@ -6,81 +6,82 @@
  ********************************************************************************/
 
 /*globals CustomEvent*/
-import actions from '@cocreate/actions';
+import actions from "@cocreate/actions";
 // import render from '@cocreate/render';
-import { matches } from './validations/matches';
-import { required } from './validations/required';
-import { unique } from './validations/unique';
-import { subdomain } from './validations/subdomain';
-import { length } from './validations/length';
-import { email } from './validations/email';
+import { matches } from "./validations/matches";
+import { required } from "./validations/required";
+import { unique } from "./validations/unique";
+import { subdomain } from "./validations/subdomain";
+import { length } from "./validations/length";
+import { email } from "./validations/email";
 
 const validations = new Map();
-validations.set('matches', matches);
-validations.set('required', required);
-validations.set('subdomain', subdomain);
-validations.set('unique', unique);
-validations.set('length', length);
-validations.set('email', email);
+validations.set("matches", matches);
+validations.set("required", required);
+validations.set("subdomain", subdomain);
+validations.set("unique", unique);
+validations.set("length", length);
+validations.set("email", email);
 
-const selectors = '[matches], [required], [unique], [subdomain], [length], [email]';
+const selectors =
+	"[matches], [required], [unique], [subdomain], [length], [email]";
 
-async function validate(btn, params) {
-    let validateElements;
-    let validateSelectors = [];
-    let failedElements = [];
+async function validate(action) {
+	let btn = action.element;
+	let params = action.params;
+	let validateElements;
+	let validateSelectors = [];
+	let failedElements = [];
 
-    if (params) {
-        let selector = params
-        for (let [validation] of validations) {
-            let vSelctor = `${selector}[${validation}]`;
-            validateSelectors.push(vSelctor);
-        }
-        let validSelectors = validateSelectors.join(', ');
-        validateElements = document.querySelectorAll(validSelectors);
-        if (!validateElements.length) {
-            let element = document.querySelector(selector);
-            if (element)
-                validateElements = element.querySelectorAll(selectors);
-        }
-    } else {
-        let element = btn.closest('form');
-        validateElements = element.querySelectorAll(selectors);
-    }
+	if (params) {
+		let selector = params;
+		for (let [validation] of validations) {
+			let vSelctor = `${selector}[${validation}]`;
+			validateSelectors.push(vSelctor);
+		}
+		let validSelectors = validateSelectors.join(", ");
+		validateElements = document.querySelectorAll(validSelectors);
+		if (!validateElements.length) {
+			let element = document.querySelector(selector);
+			if (element) validateElements = element.querySelectorAll(selectors);
+		}
+	} else {
+		let element = btn.closest("form");
+		validateElements = element.querySelectorAll(selectors);
+	}
 
+	for (let [validation, func] of validations) {
+		let elements = [];
+		for (let element of validateElements) {
+			// let validationSelectors = element.getAttribute('validate').replace(/ /g, '').split(',');
+			if (element.hasAttribute(validation)) {
+				elements.push(element);
+			}
+		}
+		if (elements.length) {
+			let failedEls = await func(elements);
+			if (failedEls) failedElements.push(failedEls);
+		}
+	}
+	for (let element of validateElements) {
+		let isValid = element.getAttribute("validation");
+		if (isValid == "false") failedElements.push(element);
+	}
 
-    for (let [validation, func] of validations) {
-        let elements = [];
-        for (let element of validateElements) {
-            // let validationSelectors = element.getAttribute('validate').replace(/ /g, '').split(',');
-            if (element.hasAttribute(validation)) {
-                elements.push(element);
-            }
-        }
-        if (elements.length) {
-            let failedEls = await func(elements);
-            if (failedEls)
-                failedElements.push(failedEls);
-        }
-    }
-    for (let element of validateElements) {
-        let isValid = element.getAttribute('validation');
-        if (isValid == 'false')
-            failedElements.push(element);
-    }
-
-    if (failedElements.length == 0) {
-        document.dispatchEvent(new CustomEvent('validate', {
-            detail: {}
-        }));
-    }
+	if (failedElements.length == 0) {
+		btn.dispatchEvent(
+			new CustomEvent("validate", {
+				detail: {}
+			})
+		);
+	}
 }
 
 actions.init({
-    name: "validate",
-    callback: (action) => {
-        validate(action.element, action.params);
-    }
+	name: "validate",
+	callback: (action) => {
+		validate(action);
+	}
 });
 
 export default { validations };
